5823272e02811b8bd1948dd85c8f5fa347e97e7b
Rupsnigdha <83987752+Rupsnigdha@users.noreply.github.com>
2023-10-16 23:00:26 +0530

    ADD: h1 heading levels

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -22,7 +22,7 @@
 							<svg xmlns="http://www.w3.org/2000/svg" height="1.5em" viewBox="0 0 448 512" class="fill-white"><path d="M0 96C0 78.3 14.3 64 32 64H416c17.7 0 32 14.3 32 32s-14.3 32-32 32H32C14.3 128 0 113.7 0 96zM0 256c0-17.7 14.3-32 32-32H416c17.7 0 32 14.3 32 32s-14.3 32-32 32H32c-17.7 0-32-14.3-32-32zM448 416c0 17.7-14.3 32-32 32H32c-17.7 0-32-14.3-32-32s14.3-32 32-32H416c17.7 0 32 14.3 32 32z"/></svg>
 						</label>
 					</div>
-					<div class="flex-1 px-2 mx-2 text-4xl font-black text-white"><a href="/">nhk</a></div>
+					<h1 class="flex-1 px-2 mx-2 text-4xl font-black text-white"><a href="/">nhk</a></h1>
 					<div class="flex-none hidden lg:block">
 						<ul class="menu menu-horizontal text-white">
 							{#each navbarItems as navbarItem}
